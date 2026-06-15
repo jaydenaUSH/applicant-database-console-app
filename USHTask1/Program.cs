80d7fb72dfb5fba8ff505cd8c24ac655f876f6cb
unknown <jayden.aurelus@ushunger.com>
2026-06-15 18:55:17 +0000

    DB validation + loop

--- USHTask1/Program.cs
+++ USHTask1/Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.IO.Enumeration;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using USHTask1.Models;
 using USHTask1.Services;
@@ -7,12 +8,40 @@ var config = new ConfigurationBuilder()
     .SetBasePath(Directory.GetCurrentDirectory())
     .AddJsonFile("appsettings.json")
     .Build();
-//DB configs
+
+Console.log("Applcation running")
+
+var validationFailed = false;
+//DB configs and setup
 var options = new DbContextOptionsBuilder<InternDbContext>()
     .UseSqlServer(config.GetConnectionString("DefaultConnection"))
     .Options;
-
 using var db = new InternDbContext(options);
+var connectable = await db.Database.CanConnect(());
+if(connectable){
+    console.log("Database connection available")
+        validationFailed = true;
+    } else { 
+    Console.log("Database connection unavailable.")
+        validationFailed = true; }
+
+//Check for the required folders and make sure R-W access
+var output;
+var log;
+
+//
+
 var report = new Report(db);
+
 await report.Generate();
+
+// Run this code every hour and once before then
+Task.Delay(TimeSpan.FromHours(1));
+{
+    await report.Generate();
+
+//Save and output the report in CSV
+fileName = "reportStuffs" 
+        Console.log("Number of records processesd: " +  )
+}
 Console.ReadLine();
